fda2532c492ea09815f589bff0b00c93152f66fb
ana-ano <anano.jolokhava@gau.edu.ge>
2026-06-03 13:09:11 +0400

    commit

--- movie_site/index.php
+++ movie_site/index.php
@@ -1,21 +1,26 @@
 <?php
 session_start();
 
-$base = "D:/xampp/htdocs/Lecture_PHP_2026/movie_site/";
+$base = __DIR__ . "/";
 $page = $_GET['page'] ?? 'home';
 
 if ($page === 'login' || $page === 'register' || $page === 'logout') {
     $_GET['action'] = $page;
     require_once $base . "app/controllers/AuthController.php";
+
 } elseif ($page === 'movie') {
     $_GET['action'] = 'show';
     require_once $base . "app/controllers/MovieController.php";
+
 } elseif ($page === 'movies') {
     require_once $base . "app/controllers/MovieController.php";
+
 } elseif ($page === 'favorites') {
     require_once $base . "app/controllers/FavoriteController.php";
+
 } elseif ($page === 'admin') {
     require_once $base . "app/controllers/AdminController.php";
+
 } else {
     require_once $base . "app/controllers/MovieController.php";
 }
